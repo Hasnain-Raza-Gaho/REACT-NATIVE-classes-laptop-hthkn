7289a0632ea22428a57b1f9ab3c0f3109d6620ce
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-16 13:47:28 +0500

    done

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [name, setName] = useState({name: 'Hasnain', edu: 'BS Hons'});
+  const updateInfo = () => {
+    setName({
+      name: 'Asad',
+      edu: 'M.Phil',
+    });
+  };
   return (
     <View style={styles.container}>
-    <Button title='Alert' color="#eee" />
+      <Text>
+        My Name Is {name.name} and my education is {name.edu}
+      </Text>
+      <Button title="Change" color="red" onPress={() => updateInfo()} />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    alignItems: 'center'
-    ,
-    justifyContent: 'center'
-  }
-})
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
