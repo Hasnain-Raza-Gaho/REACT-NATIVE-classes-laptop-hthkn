3e15ef7755ffa77bcd8eb992a2e87810ffd03b6b
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-16 14:33:39 +0500

    done

--- App.js
+++ App.js
@@ -2,11 +2,31 @@ import {StyleSheet, Text, View, Button, TextInput, Alert} from 'react-native';
 import React, {useState} from 'react';
 
 const App = () => {
-  const [name, setName] = useState('Hasnain');
+  const [data, setData] = useState(0);
+
+  const info = {
+    '0': 'this is zero',
+    '1': 'this is one',
+    '2': 'this is two',
+    '3': 'this is three',
+    '4': 'this is four',
+    '5': 'this is five',
+    '6': 'this is six',
+    '7': 'this is seven',
+    '8': 'this is eight',
+    '9': 'this is nine',
+  };
 
   return (
     <View style={styles.container}>
-      <Button title="Add Item" color="black" onPress={()=> Alert.alert('Error', "Sorry Can't Add Item")} />
+      <Button
+        title="Add Item"
+        color="black"
+        onPress={() => setData(Math.floor(Math.random() * 10))}
+
+      />
+      <Text>{data}</Text>
+      <Text>{info[data]}</Text>
     </View>
   );
 };
