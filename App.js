3ce4f97153bfe9df718d7b3d2ce24d183159f929
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-19 12:04:26 +0500

    done

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
-import {StyleSheet, Text, View, Button, TextInput, Alert} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import React, {useState} from 'react';
 
 const App = () => {
   const [name, setName] = useState('');
   const [age, setAge] = useState();
   const [gender, setGender] = useState('');
+  function dismiss() {
+    console.log('clicked..');
+    Keyboard.dismiss();
+  }
 
   return (
-    <View style={styles.container}>
-      <TextInput />
+    <TouchableWithoutFeedback onPress={() => dismiss()}>
+      <View style={styles.container}>
+        <TextInput />
 
-      <Button
-        title="Add Item"
-        color="black"
-        onPress={() =>
-          Alert.alert('Error', "Sorry Can't Add Item", [
-            {text: 'Yes', onPress: () => console.log('click on yess')},
-            {text: 'No', onPress: () => console.log('click on No..... Never!')},
-          ])
-        }
-      />
-    </View>
+        <Button
+          title="Add Item"
+          color="black"
+          onPress={() =>
+            Alert.alert('Error', "Sorry Can't Add Item", [
+              {text: 'Yes', onPress: () => console.log('click on yess')},
+              {
+                text: 'No',
+                onPress: () => console.log('click on No..... Never!'),
+              },
+            ])
+          }
+        />
+        <TextInput placeholder="hello type here" style={styles.input} />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -31,5 +50,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: 'grey',
+    width: '100%',
   },
 });
