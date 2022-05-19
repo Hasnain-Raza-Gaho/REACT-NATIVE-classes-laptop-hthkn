34abfcc7e2b16b6e79b64ee61e2ed5920b99be56
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-19 11:53:12 +0500

    done

--- App.js
+++ App.js
@@ -2,11 +2,24 @@ import {StyleSheet, Text, View, Button, TextInput, Alert} from 'react-native';
 import React, {useState} from 'react';
 
 const App = () => {
-  const [name, setName] = useState('Hasnain');
+  const [name, setName] = useState('');
+  const [age, setAge] = useState();
+  const [gender, setGender] = useState('');
 
   return (
     <View style={styles.container}>
-      <Button title="Add Item" color="black" onPress={()=> Alert.alert('Error', "Sorry Can't Add Item")} />
+      <TextInput />
+
+      <Button
+        title="Add Item"
+        color="black"
+        onPress={() =>
+          Alert.alert('Error', "Sorry Can't Add Item", [
+            {text: 'Yes', onPress: () => console.log('click on yess')},
+            {text: 'No', onPress: () => console.log('click on No..... Never!')},
+          ])
+        }
+      />
     </View>
   );
 };
