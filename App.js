b81ef70017955b4053001105ee41723d6471df86
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-20 13:14:05 +0500

    done

--- App.js
+++ App.js
@@ -2,41 +2,16 @@ import {StyleSheet, Text, View, Button} from 'react-native';
 import React from 'react';
 
 const App = () => {
-  const [name, setname] = React.useState(0);
-
-  React.useEffect(() => {
-    if(name>10){
-
-      console.warn('your DOM is updating...');
-    }
-  });
-
   return (
-    <View style={styles.container}>
-      <Text>{name}</Text>
-      <Button title="update" onPress={() => setname(name + 1)} />
+    <View style={{flex: 1, backgroundColor: 'lemon'}}>
+      <View style={{flex: 1, backgroundColor: 'red'}}></View>
+      <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+      <View style={{flex: 1, backgroundColor: 'grey'}}></View>
+      <View style={{flex: 1, backgroundColor: '#e43'}}></View>
+      <View style={{flex: 1, backgroundColor: '#fab'}}></View>
+      <View style={{flex: 4, backgroundColor: 'yellow'}}></View>
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  btn: {
-    backgroundColor: 'yellow',
-    padding: 20,
-    marginTop: 30,
-    alignItems: 'center',
-    borderWidth: 3,
-    borderColor: 'green',
-    borderRadius: 20,
-  },
-  input: {
-    backgroundColor: 'grey',
-    padding: 10,
-  },
-});
