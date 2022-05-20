2535ac8a0d845d7d9f111708eaa7772aa7909f4d
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-20 19:28:08 +0500

    done

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import CustomButton from './customButton';
+import React, {useState} from 'react';
+import {
+  TextInput,
+  Text,
+  View,
+  ActivityIndicator,
+  Button,
+  Alert,
+} from 'react-native';
+
 import styles from './styles';
 
 const App = () => {
-  const [name, setName] = React.useState(false);
+  const [name, setName] = useState('');
+  const [show, setShow] = useState(false);
 
+  const clickMe = () => {
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+      Alert.alert('info', 'Thanks of Coming ' + name);
+    }, 2000);
+  };
   return (
     <View style={styles.container}>
-      <CustomButton name="Click Me" />
-      <CustomButton name="Dont Click Me" />
+      <TextInput
+        placeholder="add text here"
+        onChangeText={text => setName(text)}
+        style={styles.input}
+      />
+      <ActivityIndicator size={'large'} color="red" animating={show} />
+      <Button title="Show" onPress={() => clickMe()} />
     </View>
   );
 };
