fd0eaae2880a1f2fd2a96f463926ecd899209008
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-20 17:11:11 +0500

    done

--- App.js
+++ App.js
@@ -3,20 +3,10 @@ import {Text, View, Switch} from 'react-native';
 import styles from './styles';
 
 const App = () => {
-  const [isEnabled, setIsEnabled] = React.useState(false);
-  const toggleSwitch = () => {
-    console.log('clicked');
-    setIsEnabled(prevState => !prevState);
-  };
+  
   return (
     <View style={styles.container}>
-      <Text>Switch Tutorial</Text>
-      <Switch
-        trackColor={{false: 'grey', true: 'Green'}}
-        thumbColor={isEnabled ? 'red' : 'pink'}
-        onValueChange={() => toggleSwitch()}
-        value={isEnabled}
-      />
+
     </View>
   );
 };
