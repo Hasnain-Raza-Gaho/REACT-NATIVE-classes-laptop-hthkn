1b465676cd6c544e86f2e357f506d5d423fdf522
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-25 12:31:45 +0500

    done

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
 const App = () => {
-
   return (
     <View style={styles.container}>
-
+      <Image
+        source={require('./src/assets/image1.jpg')}
+        style={{height: 200, width: 200, tintColor: 'yellow'}}
+      />
     </View>
   );
 };
--- styles.js
+++ styles.js
@@ -4,23 +4,11 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'grey',
     padding: 20,
-    margin: 40,
-    borderBottomWidth: 20,
-    borderTopWidth: 20,
-    borderWidth: 10,
-    borderBottomColor: 'red',
-    borderTopColor: 'grey',
-    borderRightColor: 'green',
-    borderLeftColor: 'blue',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 90,
-    borderColor: 'black',
-    borderWidth: 40,
-    opacity: 0.4
-    
-
+    justifyContent: 'center',
+    alignItems: 'center'
+   
   },
 });
 
