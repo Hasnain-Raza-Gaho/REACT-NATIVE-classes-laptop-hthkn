2979c7f9a592f287f88bf77264a9b294c59a66f7
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-05-25 10:10:40 +0500

    done

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
 import React, {useState} from 'react';
-import {View, Text, ScrollView, RefreshControl} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import styles from './styles';
 
 const App = () => {
-  const [refresh, setRefresh] = useState(false);
-
-  const pullMe = () => {
-    setRefresh(true);
-    setTimeout(() => {
-      setRefresh(false);
-    }, 3000);
-  };
 
   return (
     <View style={styles.container}>
-      <ScrollView
-        refreshControl={
-          <RefreshControl refreshing={refresh} onRefresh={() => pullMe()} />
-        }>
-        <Text>Pull to Refresh</Text>
+<Text style={styles.text}>Hello Everyone... How Are You ? I Hope You Are Fine and Doing Well, I am just writing a Paragraph.</Text>
+
+<ScrollView style={styles.scroller}>
+
+        <Text style={{fontFamily: 'normal'}}>  normal </Text>
+
+        <Text style={{fontFamily: 'notoserif'}}>  notoserif </Text>
+
+        <Text style={{fontFamily: 'sans-serif'}}>  sans-serif </Text>
+
+        <Text style={{fontFamily: 'sans-serif-light'}}>  sans-serif-light </Text>
+
+        <Text style={{fontFamily: 'sans-serif-thin'}}>  sans-serif-thin </Text>
+
+        <Text style={{fontFamily: 'sans-serif-condensed'}}>  sans-serif-condensed </Text>
+
+        <Text style={{fontFamily: 'sans-serif-medium'}}>  sans-serif-medium </Text>
+
+        <Text style={{fontFamily: 'serif'}}>  serif </Text>
+
+        <Text style={{fontFamily: 'Roboto'}}>  Roboto </Text>
+
+        <Text style={{fontFamily: 'monospace'}}>  monospace </Text>        
+
       </ScrollView>
     </View>
   );
--- styles.js
+++ styles.js
@@ -7,7 +7,20 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor: 'yellow',
     marginTop: 20,
+    justifyContent: 'center',
+    alignItems:'center'
   },
+  text:{
+    fontFamily: 'notoserif',
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    letterSpacing: 3,
+    textAlign: 'right',
+    textDecorationLine: 'line-through',
+    textDecorationColor: 'red',
+    textTransform: 'lowercase',
+    fontSize: 30
+  }
  
 });
 
